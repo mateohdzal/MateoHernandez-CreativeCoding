96ed62976a60b63dfd44c5135b7d034081971730
Kevin Siwoff <info@kevinsiwoff.com>
2016-10-17 16:23:40 -0400

    Adds comments to Moire pattern

--- Week 3/MoirePattern/sketch.js
+++ Week 3/MoirePattern/sketch.js
@@ -1,3 +1,10 @@
+/**
+** Kevin's notes:  This is a very cool sketch.  I really like what you're doing here visually.
+Comments inline...
+*/
+
+//Consider clearer variable names.  a1 a2, etc.  are not descriptive of their visual role.  Don't
+//be afraid to have a longer variable name as long as it represents its purpose in the sketch.
 var a1=400;
 var a2=300;
 var a3=450;
@@ -5,6 +12,9 @@ var a4=10;
 var c1=20;
 function setup() {
   createCanvas(1000,1000);
+//consider moving this for loop into your draw loop.
+//You'll then need to reset a1,a2,a3,and a4 to their default values above.
+//otherwise every draw call will add 3,5,and 8 to each variable respectively.
   for(var i=1;i<=50;i++){
   beginShape();
 	vertex(a1,100);
@@ -38,6 +48,9 @@ function draw() {
 // triangle(i1-30, i1+75, i1+58, i1-20, i1+86, i1+75);
 // } 
 
+//These shapes are great, but consider replacing the hard coded numbers with
+//variables referencing proportion of width and height of your canvas.  What would happen
+//to the sketch if I resized the canvas?
 for(var i1=400;i1<=800;i1=i1+4){
   line(i1,300,i1-280,500);
 } 
